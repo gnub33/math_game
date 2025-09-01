45bddec8ea6a3a588809bf0ec088166655fca2d0
dknight <dknight@code.wm.edu>
2025-09-01 03:13:56 -0400

    refactoring code

--- math_game/Program.cs
+++ math_game/Program.cs
@@ -1,9 +1,70 @@
-﻿namespace math_game;
+﻿using System.ComponentModel.Design;
 
-class Program
+Console.WriteLine("Please type your name");
+var name = Console.ReadLine();
+var date = DateTime.UtcNow;
+
+Menu(name, date);
+
+void MultiplicationGame(string messaage)
 {
-    static void Main(string[] args)
+    Console.WriteLine(messaage);
+}
+
+void DivisionGame(string message)
+{
+    Console.WriteLine(message);
+}
+
+void SubtractionGame(string message)
+{
+    Console.WriteLine(message);
+}
+
+void AdditionGame(string message)
+{
+    Console.WriteLine(message);
+}
+
+// aparently visual studio has a feature to auto generate a method from context. Refactoring?
+void Menu(string? name, DateTime date)
+{
+    Console.WriteLine("--------------------------------------------");
+    Console.WriteLine(
+        $"Hello {name.ToUpper()}. This is a math game. It's currently {date.DayOfWeek}, which means it's time to code!");
+    Console.WriteLine(@$"What game would you like to play today? Choose from the options below:
+A - Addition
+S- Subtraction
+M - Multiplication
+D - Division
+Q - Quit");
+    Console.WriteLine("--------------------------------------------");
+
+    var gameSelected = Console.ReadLine();
+
+    switch (gameSelected.Trim().ToLower())
     {
-        Console.WriteLine("Hello, World!");
+        case "a":
+            AdditionGame("Addiction game selected");
+            break;
+        case "s":
+            SubtractionGame("Subtraction game selected");
+            break;
+        case "m":
+            MultiplicationGame("Multiplication game selected");
+            break;
+        case "d":
+            DivisionGame("Division game selected");
+            break;
+        case "q":
+            Console.WriteLine("Bye!");
+            Environment.Exit(1);
+            break;
+        default:
+            Console.WriteLine("You did not enter a valid option");
+            Environment.Exit(1);
+            break;
+
     }
+        
 }
